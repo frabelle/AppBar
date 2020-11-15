686b1ce8dbefc9541c1efd1d1546d6f9a10fe083
frabelle <renchifrabelle@gmail.com>
2020-11-15 01:05:55 -0600

    Second Commit

--- app/src/main/java/com/example/appbar/MainActivity.java
+++ app/src/main/java/com/example/appbar/MainActivity.java
@@ -36,19 +36,19 @@ public class MainActivity extends AppCompatActivity{
         int id = item.getItemId();
 
         if (id == R.id.share) {
-            Toast.makeText(getApplicationContext(), "Seleccionaste la opción de Compartir",
+            Toast.makeText(getApplicationContext(), "Seleccionaste la opción de Compartir!",
                     Toast.LENGTH_SHORT).show();
         } else if (id == R.id.aboutus) {
-            Toast.makeText(getApplicationContext(), "Seleccionaste la opción de Acerca de",
+            Toast.makeText(getApplicationContext(), "Opción de Acerca de",
                     Toast.LENGTH_SHORT).show();
         } else if (id == R.id.exit) {
-            Toast.makeText(getApplicationContext(), "Seleccionaste la opción de Salir",
+            Toast.makeText(getApplicationContext(), "Seleccionaste la opción de Salir:(",
                     Toast.LENGTH_SHORT).show();
         }else if (id == R.id.search) {
-            Toast.makeText(getApplicationContext(), "Seleccionaste la opción de Buscar",
+            Toast.makeText(getApplicationContext(), "Seleccionasteeeee la opción de Buscar",
                     Toast.LENGTH_SHORT).show();
         }else if (id == R.id.ajustes) {
-            Toast.makeText(getApplicationContext(), "Seleccionaste la opción de Ajustes",
+            Toast.makeText(getApplicationContext(), "Seleccionaste Ajustes",
                     Toast.LENGTH_SHORT).show();
         }
 
