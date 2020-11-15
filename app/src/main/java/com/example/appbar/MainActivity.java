9298df74cd0d05ade8adff9f010845cf636700fa
frabelle <renchifrabelle@gmail.com>
2020-11-15 01:26:09 -0600

    Something it's not working

--- app/src/main/java/com/example/appbar/MainActivity.java
+++ app/src/main/java/com/example/appbar/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
     }
 
     @Override
